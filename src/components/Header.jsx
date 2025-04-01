e427917a9954f20ee6387dc7efd2a3dd4e3ae32b
hoangnguyen106 <nguyenhoangnhom4@gmail.com>
2025-04-01 21:12:26 +0700

    complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,46 @@
-import PropType from 'prop-types'
-import React from 'react'
+import PropTypes from "prop-types";
+import { useState } from "react";
 
-function Header({ onSearch }) {
-    const [textSearch, setTextSearch] = React.useState('')
-    return (
-        <div className="p-4 bg-black flex items-center justify-between">
-            <div className="flex items-center space-x-4">
-                <h1 className="text-[30px] uppercase font-bold text-red-700">Movie</h1>
-                <nav className="flex items-center space-x-4">
-                    <a className="text-white" href="#">Home</a>
-                    <a className="text-white" href="#">About</a>
-                    <a className="text-white" href="#">Contact</a>
-                </nav>
-            </div>
-            <div className="flex items-center space-x-4">
-                <input type="text" placeholder="Search" className="p-3 text-black" onChange={(e) => setTextSearch(e.target.value)} value={textSearch} />
-                <button className="bg-red-600 text-white p-2 rounded-md" onClick={() => onSearch(textSearch)}>Search</button>
-            </div>
-        </div>
+const Header = ({ onSearch }) => {
+  const [search, setSearch] = useState("");
 
-    )
-}
+  return (
+    <div className="p-4 flex justify-between  fixed top-0 left-0 w-full z-[9999]  bg-black">
+      <div className="flex items-center gap-8">
+        <h1 className="text-[30px] uppercase text-red-700 font-bold">Movie</h1>
+        <nav className="hidden md:flex items-center space-x-5">
+          <a href="#" className="hover:text-red-700">
+            Home
+          </a>
+          <a href="#" className="hover:text-red-700">
+            About
+          </a>
+          <a href="#" className="hover:text-red-700">
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="flex items-center space-x-5">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border border-gray-300 p-2 text-black"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button
+          className="bg-red-700 text-white px-3 py-1 rounded-lg"
+          onClick={() => onSearch(search)}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
 
 Header.propTypes = {
-    onSearch: PropType.func,
-}
+  onSearch: PropTypes.func.isRequired,
+};
 
-export default Header
+export default Header;
